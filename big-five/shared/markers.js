f427a86d1fa549e2efb7e386a2d60ad0befbee39
Jordan <jbrockerrudow@gmail.com>
2022-12-03 10:10:41 -0600

    Add Extraversion Data

---
+++ big-five/shared/markers.js
@@ -0,0 +1,31 @@
+MARKERS = [
+    {
+        "name": "Extraversion",
+        "pos": [
+            "Am the life of the party.",
+            "Feel comfortable around people.",
+            "Start conversations.",
+            "Talk to a lot of different people at parties.",
+            "Don't mind being the center of attention.",
+            "Make friends easily.",
+            "Take charge.",
+            "Know how to captivate people.",
+            "Feel at ease with people.",
+            "Am skilled in handling social situations."
+        ],
+        "neg": [
+            "Don't talk a lot.",
+            "Keep in the background.",
+            "Have little to say.",
+            "Don't like to draw attention to myself.",
+            "Am quiet around strangers.",
+            "Find it difficult to approach others.",
+            "Often feel uncomfortable around others.",
+            "Bottle up my feelings.",
+            "Am a very private person.",
+            "Wait for others to lead the way.",
+        ]
+    },
+
+
+];
